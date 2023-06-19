80a1b796daf41c9f12ce6143cb757a2398be14e8
Abhiraj Banerjee <110682893+Wriki-tmsl@users.noreply.github.com>
2023-06-19 09:30:37 +0530

    Add files via upload

---
+++ LearnPrint.java
@@ -0,0 +1,11 @@
+public class LearnPrint {
+    public static void main(String arg[])
+
+    {
+    ///System.out.println() print and move to new line #supports any datatype 
+    //   System.out.println(''str''+ any data type+ any data type )
+   //System.out.printf(); same as c language %d%f%c%s to print variables %x hexadec
+    
+    }
+
+}
---
+++ NewStr.java
@@ -0,0 +1,15 @@
+public class NewStr {
+    public static void main (String[] args)
+    { String str1 ="Java ....";
+    System.out.printf("%s\n",str1);
+    String str2= new String("JAva nEw Str..");
+    System.out.printf("%s\n",str2);
+    char c[]={'H','e','l','l','f','i','r','e'};
+    
+    String str3=new String(c);
+    System.out.printf("%s\n",str3);
+    byte b []= {3,67,123,113,45,90,45};
+    String str4=new String(b);
+    System.out.printf("%s'n",str4);
+    }
+}
